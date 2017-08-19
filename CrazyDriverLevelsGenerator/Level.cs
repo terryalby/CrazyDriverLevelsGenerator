c97dfde6b5e58d474ae3a24853c667b3d6cf71ba
Alberti Terence <teralb@xmedia.loc>
2017-08-20 00:32:47 +0200

    first commit

---
+++ CrazyDriverLevelsGenerator/Level.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrazyDriverLevelsGenerator
+{
+
+    public class Entry
+    {
+        public string Distance { get; set; }
+        public string Object { get; set; }
+        public string OriginX { get; set; }
+    }
+
+    public class Level
+    {
+        public string LevelName { get; set; }
+        public string LevelId { get; set; }
+        public List<Entry> Data { get; set; }
+    }
+}
---
+++ CrazyDriverLevelsGenerator/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Newtonsoft;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace CrazyDriverLevelsGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var level = GenerateLevel(40, 30000, "1", "level1.json");
+            PrintLevelInJsonFormat(level);
+            Console.Read();
+        }
+
+        // difficulty from 1 to 100
+        static Level GenerateLevel(int diffculty, int ticks, string levelId, string levelName)
+        {
+            var random = new Random();
+            var level = new Level();
+            var currentTick = 1;
+
+            level.LevelId = levelId;
+            level.LevelName = levelName;
+            level.Data = new List<Entry>();
+
+            while (currentTick < ticks)
+            {
+                var entry = new Entry();
+
+                currentTick = (int) random.NextDouble() * (currentTick+200-diffculty - currentTick) + currentTick+200-diffculty;
+
+                entry.Distance = currentTick.ToString();
+                entry.OriginX = ((int)(random.NextDouble() * (150 - 0))).ToString();
+                entry.Object = random.NextDouble() > ((double) diffculty / 100) ?  "Ammunition" : "RedCar";
+
+                level.Data.Add(entry);
+            }
+
+            var endEntry = new Entry();
+
+            endEntry.Distance = (currentTick + 1).ToString();
+            endEntry.OriginX = "0";
+            endEntry.Object = "End";
+
+            level.Data.Add(endEntry);
+
+            return level;
+        }
+
+        static void PrintLevelInJsonFormat(Level level)
+        {
+            Console.Write(JsonConvert.SerializeObject(level));
+        }
+    }
+}
